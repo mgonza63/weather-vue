4fe051cacd83bed386bead6ba43744d88cfedf3e
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-13 22:25:49 -0500

    initial commit with vue cli

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return  {
+    api_key: '6e314ca29224f22bfcbc9e31488128dc'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
